5cd4198697644edc5d9f92f89c0eb5d389873401
unknown <dev.juan.eloper@gmail.com>
2024-03-25 22:10:40 -0600

    Cleaning

--- Task_App_Client/src/components/TaskHome/index.tsx
+++ Task_App_Client/src/components/TaskHome/index.tsx
@@ -1,7 +1,4 @@
-import React, { useState } from 'react';
-import { Button, Text } from 'react-native';
-import { connect } from "react-redux";
-import { showAlert } from '../../redux/actions/alert.action';
+import React from 'react';
 import CardTask from '../CardTask';
 
 
@@ -14,16 +11,4 @@ const TaskHome: React.FC = (props: any) => {
     )
 }
 
-const mapStateToProps = (state: any) => ({
-    isShow: state.alert.isShow,
-    // message: state.alert.message,
-    // type: state.alert.type,
-    // callback: state.alert.callback,
-    // title: state.alert.title
-})
-
-const mapDispatchToProps = (dispatch: any) => ({
-    _showAlert: (title: string, message: string, isShow: boolean) => dispatch(showAlert(title, message, isShow))
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(TaskHome);
+export default TaskHome;
