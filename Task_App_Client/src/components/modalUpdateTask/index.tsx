9c95b6f4995c043330824c4a5c6c8e2845209001
unknown <dev.juan.eloper@gmail.com>
2024-03-26 20:54:23 -0600

    Cleaning

--- Task_App_Client/src/components/modalUpdateTask/index.tsx
+++ Task_App_Client/src/components/modalUpdateTask/index.tsx
@@ -38,7 +38,7 @@ const ModalUpdate = (props: Props) => {
                 }}>
                 <View style={styles.centeredView}>
                     <View style={styles.modalView}>
-                        <Text style={styles.modalText}>Hello World!</Text>
+                        <Text style={styles.modalText}>Editar task</Text>
                         <TextInput value={value} onChangeText={handleChange} />
                         <Pressable
                             style={[styles.button, styles.buttonClose]}
